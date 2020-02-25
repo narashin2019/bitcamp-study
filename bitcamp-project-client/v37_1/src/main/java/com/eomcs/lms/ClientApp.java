d40195d1aa5a3f72db247c8ddfb05e5bdb0b1666
gomgugu <narashin547@gmail.com>
2020-02-25 19:28:50 +0900

    200225 학원

--- bitcamp-project-client/v37_1/src/main/java/com/eomcs/lms/ClientApp.java
+++ bitcamp-project-client/v37_1/src/main/java/com/eomcs/lms/ClientApp.java
@@ -1,143 +1,19 @@
-// LMS 클라이언트
-package com.eomcs.lms;
+# 37_5 - Application Server 구조로 변경: 서버 종료 명령 처리하기
 
-import java.io.PrintStream;
-import java.net.Socket;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import com.eomcs.util.Prompt;
+## 학습목표
 
-public class ClientApp {
+- Application Server 아키텍처의 구성과 특징을 이해한다.
+- 통신 프로토콜 규칙에 따라 동작하는 클라이언트를 만들 수 있다.
+- 멀티 스레드 환경에서 스레드풀의 동작을 제어할 수 있다.
 
-  Scanner keyboard = new Scanner(System.in);
-  Prompt prompt = new Prompt(keyboard);
+## 실습 소스 및 결과
 
-  Deque<String> commandStack;
-  Queue<String> commandQueue;
+- src/main/java/com/eomcs/lms/ClientApp.java 변경
 
-  public ClientApp() throws Exception {
-    commandStack = new ArrayDeque<>();
-    commandQueue = new LinkedList<>();
-  }
+## 실습  
 
-  public void service() {
+### 훈련1: '/server/stop' 명령을 처리하라.
 
-    while (true) {
-      String command;
-      command = prompt.inputString("\n명령> ");
+- com.eomcs.lms.ClientApp 변경
+  - 사용자가 '/server/stop'을 입력했을 때 서버가 종료 작업을 즉시 할 수 있도록 두 번 요청한다.
 
-      if (command.length() == 0)
-        continue;
-
-      if (command.equals("history")) {
-        printCommandHistory(commandStack.iterator());
-        continue;
-      } else if (command.equals("history2")) {
-        printCommandHistory(commandQueue.iterator());
-        continue;
-      } else if (command.equals("quit")) {
-        break;
-      }
-
-      commandStack.push(command);
-      commandQueue.offer(command);
-
-      processCommand(command);
-    }
-    keyboard.close();
-  }
-
-  private void processCommand(String command) {
-    // 명령어 형식을 변경!
-    // [기존 방식]
-    // => 예) /board/list
-    // [새 방식]
-    // => 예) bitcamp://서버주소:포트번호/board/list
-    //
-    String host = null;
-    int port = 9999;
-    String servletPath = null;
-
-    // 명령어를 분석하여 서버주소와 포트번호, 실행시킬 작업명을 분리한다.
-    try {
-      if (!command.startsWith("bitcamp://")) {
-        throw new Exception("명령어 형식이 옳지 않습니다!");
-      }
-
-      // System.out.println(command);
-      // command 예) bitcamp://localhost:9999/board/list
-
-      String url = command.substring(10);
-      // => localhost:9999/board/list
-
-      // System.out.println(url);
-
-      int index = url.indexOf('/'); // 14
-      String[] str = //
-          url.substring(0, index) // localhost:9999
-              .split(":"); // {"localhost", "9999"}
-
-      host = str[0];
-      if (str.length == 2) {
-        port = Integer.parseInt(str[1]);
-      }
-      // System.out.printf("=> %s:%d\n", host, port); // => localhost:9999
-
-      servletPath = url.substring(index);
-      System.out.printf("=> %s\n", servletPath); // => /board/list
-
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      return;
-    }
-
-    // 서버에 연결한다.
-    try (Socket socket = new Socket(host, port);
-        PrintStream out = new PrintStream(socket.getOutputStream());
-        Scanner in = new Scanner(socket.getInputStream())) {
-
-      // 서버에 명령을 보낸다.
-      out.println(servletPath);
-      out.flush();
-
-      // 서버의 응답을 읽어서 출력한다.
-      while (true) {
-        String response = in.nextLine();
-        if (response.equals("!end!")) {
-          break;
-        }
-
-        System.out.println(response);
-      }
-
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-    }
-  }
-
-  private void printCommandHistory(Iterator<String> iterator) {
-    int count = 0;
-    while (iterator.hasNext()) {
-      System.out.println(iterator.next());
-      count++;
-
-      if ((count % 5) == 0) {
-        String str = prompt.inputString(":");
-        if (str.equalsIgnoreCase("q")) {
-          break;
-        }
-      }
-    }
-  }
-
-  public static void main(String[] args) throws Exception {
-    System.out.println("클라이언트 수업 관리 시스템입니다.");
-
-    ClientApp app = new ClientApp();
-    app.service();
-  }
-}
---
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/dao/mariadb/PhotoFileDaoImpl.java
@@ -0,0 +1,68 @@
+package com.eomcs.lms.dao.mariadb;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import com.eomcs.lms.dao.PhotoFileDao;
+import com.eomcs.lms.domain.PhotoFile;
+
+public class PhotoFileDaoImpl implements PhotoFileDao {
+
+  Connection con;
+
+  public PhotoFileDaoImpl(Connection con) {
+    this.con = con;
+  }
+
+  @Override
+  public int insert(PhotoFile photoFile) throws Exception {
+    try (Statement stmt = con.createStatement()) {
+
+      int result = stmt.executeUpdate( //
+          "insert into lms_photo_file(photo_id,file_path) values(" //
+              + photoFile.getBoardNo() + ", '" + photoFile.getFilepath() //
+              + "')");
+
+      return result;
+    }
+  }
+
+
+  @Override
+  public List<PhotoFile> findAll(int boardNo) throws Exception {
+    try (Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery( //
+            "select photo_file_id, photo_id, file_path" //
+                + " from lms_photo_file" //
+                + " where photo_id=" + boardNo //
+                + " order by photo_file_id asc;")) {
+
+      ArrayList<PhotoFile> list = new ArrayList<>();
+
+      while (rs.next()) {
+        PhotoFile photoFile = new PhotoFile();
+
+        photoFile.setNo(rs.getInt("photo_file_id"));
+        photoFile.setBoardNo(rs.getInt("photo_id"));
+        photoFile.setFilepath(rs.getString("file_path"));
+
+        list.add(photoFile);
+      }
+
+      return list;
+    }
+  }
+
+  @Override
+  public int deleteAll(int boardNo) throws Exception {
+    try (Statement stmt = con.createStatement()) {
+      int result = stmt.executeUpdate( //
+          "delete from lms_photo_file" //
+              + " where photo_id=" + boardNo);
+      return result;
+    }
+  }
+
+}
