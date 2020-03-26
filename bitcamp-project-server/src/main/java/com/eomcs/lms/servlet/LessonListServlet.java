8d925b932c5b1b71edc6f2596dad731e7c2ca77c
gomgugu <narashin547@gmail.com>
2020-03-26 18:30:40 +0900

    200326

--- bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/LessonListServlet.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/LessonListServlet.java
@@ -19,57 +19,52 @@ public class LessonListServlet {
 
   @RequestMapping("/lesson/list")
   public void service(Map<String, String> params, PrintStream out) throws Exception {
- 
-    
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
     out.println("  <meta charset='UTF-8'>");
-    out.println("  <title>수업 목록</title>");
+    out.println("  <title>강의 목록</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("  <h1>수업</h1>");
-    out.println("  <a href='/lesson/addForm'>새 글</a><br>");
+    out.println("  <h1>강의</h1>");
+    out.println("  <a href='/lesson/addForm'>새 강의</a><br>");
     out.println("  <table border='1'>");
     out.println("  <tr>");
     out.println("    <th>번호</th>");
-    out.println("    <th>시작일</th>");
-    out.println("    <th>종료일</th>");
-    out.println("    <th>총수업시간</th>");
-    out.println("    <th>일수업시간</th>");
-    out.println("    <th>제목</th>");
-    out.println("    <th>내용</th>");
+    out.println("    <th>강의</th>");
+    out.println("    <th>기간</th>");
+    out.println("    <th>총강의시간</th>");
     out.println("  </tr>");
 
-    
-    
-    
-    
     List<Lesson> lessons = lessonService.list();
     for (Lesson l : lessons) {
       out.printf("  <tr>"//
-          + "<td>%d</td> ", //"
-         // + "<td><a href='/lesson/detail?no=%d'>%s</a></td> "//
-          + "<td>%s</td>", //
-          + "<td>%s</td> "
-          + "<td>%d</td>, //"
-          + "<td>%d</td>"
-          + "<td>%s</td>"
-          + "<td>%s</td>"
+          + "<td>%d</td> "//
+          + "<td><a href='/lesson/detail?no=%d'>%s</a></td> "//
+          + "<td>%s ~ %s</td> "//
+          + "<td>%d</td>"//
           + "</tr>\n", //
           l.getNo(), //
-       //   l.getNo(), //
+          l.getNo(), //
+          l.getTitle(), //
           l.getStartDate(), //
           l.getEndDate(), //
-          l.getTotalHours(), //
-          l.getDayHours(), //
-          l.getTitle(), //
-          l.getDescription()//
+          l.getTotalHours() //
+      );
+    }
+    out.println("</table>");
 
+    out.println("<hr>");
 
-    );
-  }
+    out.println("<form action='/lesson/search'>");
+    out.println("강의명: <input name='title' type='text'><br>");
+    out.println("강의 시작일: <input name='startDate' type='date'><br>");
+    out.println("강의 종료일: <input name='endDate' type='date'><br>");
+    out.println("총 강의시간: <input name='totalHours' type='number'><br>");
+    out.println("일 강의시간: <input name='dayHours' type='number'><br>");
+    out.println("<button>검색</button>");
+
     out.println("</body>");
     out.println("</html>");
-}
+  }
 }
--- bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/LessonUpdateFormServlet.java
+++ bitcamp-project-server/src/main/java/com/eomcs/lms/servlet/LessonUpdateFormServlet.java
@@ -9,17 +9,19 @@ import com.eomcs.lms.service.LessonService;
 import com.eomcs.util.RequestMapping;
 
 @Component
-public class LessonAddServlet {
+public class LessonUpdateServlet {
 
   LessonService lessonService;
 
-  public LessonAddServlet(LessonService lessonService) {
+  public LessonUpdateServlet(LessonService lessonService) {
     this.lessonService = lessonService;
   }
 
-  @RequestMapping("/lesson/add")
+  @RequestMapping("/lesson/update")
   public void service(Map<String, String> params, PrintStream out) throws Exception {
+
     Lesson lesson = new Lesson();
+    lesson.setNo(Integer.parseInt(params.get("no")));
     lesson.setTitle(params.get("title"));
     lesson.setDescription(params.get("description"));
     lesson.setStartDate(Date.valueOf(params.get("startDate")));
@@ -27,18 +29,23 @@ public class LessonAddServlet {
     lesson.setTotalHours(Integer.parseInt(params.get("totalHours")));
     lesson.setDayHours(Integer.parseInt(params.get("dayHours")));
 
-    lessonService.add(lesson);
-
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
     out.println("<meta charset='UTF-8'>");
     out.println("<meta http-equiv='refresh' content='2;url=/lesson/list'>");
-    out.println("<title>강의 입력</title>");
+    out.println("<title>강의 변경</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>강의 입력 결과</h1>");
-    out.println("<p>새 강의를 등록했습니다.</p>");
+    out.println("<h1>강의 변경 결과</h1>");
+
+    if (lessonService.update(lesson) > 0) {
+      out.println("<p>강의를 변경했습니다.</p>");
+
+    } else {
+      out.println("<p>변경에 실패했습니다.</p>");
+    }
+
     out.println("</body>");
     out.println("</html>");
   }
